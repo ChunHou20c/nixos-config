6badbd1989ec8e3e2ad3fc3e5d17e90922a6130f
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-12-30 22:59:53 +0800

    update flake and added host

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS flake by ChunHou20c";
+  description = "NixOS flake configuration by ChunHou20c";
 
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
@@ -8,7 +8,7 @@
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.lenovo-ideapad-slim-5i = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
