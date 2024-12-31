e101475456a5c40234922d124d0f349277de0d20
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-12-31 11:24:49 +0800

    move nvim config to init.vim - user only

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,27 +1,6 @@
 {
-  pkgs,
-  config,
-  ...
-}: {
-  # i3 配置，基于 https://github.com/endeavouros-team/endeavouros-i3wm-setup
-  # 直接从当前文件夹中读取配置文件作为配置内容
-
-  # wallpaper, binary file
   home.file.".config/hypr" = {
     source = ./hypr;
-    # copy the scripts directory recursively
     recursive = true;
   };
-
-  # set cursor size and dpi for 4k monitor
-  xresources.properties = {
-    "Xcursor.size" = 16;
-    "Xft.dpi" = 192;
-  };
-
-  # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
 }
--- modules/systems.nix
+++ modules/systems.nix
@@ -23,38 +23,7 @@
     viAlias = true;
 
     configure = {
-    customRC = ''
-      set number
-      set relativenumber
-      set conceallevel=0
-      syntax on
-      filetype indent on
-      set shiftwidth=2
-      let mapleader=" "
-      nnoremap <leader>gg :LazyGit<CR>
-      nnoremap <leader>l :NvimTreeToggle<CR>
-      nmap <F6> :TagbarToggle<CR>
-      source /etc/nixos/config/nvim/vim/floaterm.vim
-      filetype plugin indent on
-      
-      colorscheme tokyonight-storm
-      luafile /etc/nixos/config/nvim/lua/telescope.lua
-      luafile /etc/nixos/config/nvim/lua/nvimtree.lua
-      luafile /etc/nixos/config/nvim/lua/treesitter.lua
-      luafile /etc/nixos/config/nvim/lua/gitsigns.lua
-      luafile /etc/nixos/config/nvim/lua/lualine.lua
-      luafile /etc/nixos/config/nvim/lua/lspkind.lua
-      luafile /etc/nixos/config/nvim/lua/autotag.lua
-      luafile /etc/nixos/config/nvim/lua/luasnip.lua
-
-      lua << EOF
-      vim.defer_fn(function()
-        vim.cmd [[
-          luafile /etc/nixos/config/nvim/lua/lsp.lua
-        ]]
-      end, 70)
-      EOF
-      '';
+    customRC = '''';
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [ 
 
