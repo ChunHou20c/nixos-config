7ad2dc3ad82ecaf474274bbcd26d3f600d3836e9
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-12-31 15:22:56 +0800

    move nixvim config into its own module

--- users/chunhou/home.nix
+++ users/chunhou/home.nix
@@ -8,6 +8,7 @@
   imports = [
     ../../home/core.nix
     ../../home/hyprland
+    ../../home/nixvim
     nixvim.homeManagerModules.nixvim
   ];
 
@@ -16,90 +17,6 @@
     userEmail = "chunhouthatbornin20c@gmail.com";
   };
 
-  programs.nixvim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-
-    opts = {
-      number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      shiftwidth = 2;        # Tab width should be 2
-    };
-    globals.mapleader = " ";
-    colorschemes.tokyonight = {
-    enable = true;
-    settings.style = "storm";
-    };
-
-    plugins = {
-
-      treesitter = {
-	enable = true;
-
-	grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-	  bash
-	  json
-	  lua
-	  make
-	  markdown
-	  nix
-	  regex
-	  toml
-	  vim
-	  vimdoc
-	  xml
-	  yaml
-	  rust
-	  python
-	];
-      };
-      lualine.enable = true;
-      nvim-tree = {
-      openOnSetup = true;
-      enable = true;
-      };
-      lazygit.enable = true;
-      gitsigns.enable = true;
-      telescope.enable = true;
-      floaterm = {
-      enable = true;
-      width = 0.9;
-      height = 0.9;
-      };
-      indent-blankline.enable = true;
-      commentary.enable = true;
-    };
-
-    keymaps = [
-    {
-      mode = "n";
-      key = "<leader>l";
-      options.silent = true;
-      action = ":NvimTreeToggle<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>gg";
-      options.silent = true;
-      action = ":LazyGit<CR>";
-    }
-    {
-
-      mode = "n";
-      key = "<F12>";
-      options.silent = true;
-      action = ":FloatermToggle<CR>";
-    }
-    {
-
-      mode = "t";
-      key = "<F12>";
-      options.silent = true;
-      action = "<C-\\><C-n>:FloatermToggle<CR>";
-    }
-    ];
-  };
 
   home.packages = with pkgs; [
 
