67ec94685814c2167fc7f3dcd228b8e43540c82f
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2025-01-01 12:44:39 +0800

    added starship and kitty config

---
+++ home/starship/default.nix
@@ -0,0 +1,3 @@
+{
+  home.file.".config/starship.toml".source = ./starship.toml;
+}
--- users/chunhou/home.nix
+++ users/chunhou/home.nix
@@ -59,4 +59,10 @@
 
     };
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
 }
