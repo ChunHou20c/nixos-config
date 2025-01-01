41828e68a9bfdc18b067ed86cbf8ba4342154c0e
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2025-01-01 15:09:22 +0800

    fix gaming.nix and set terminal font

--- modules/systems.nix
+++ modules/systems.nix
@@ -49,6 +49,13 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+   console = {
+    earlySetup = true;
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+    packages = with pkgs; [ terminus_font ];
+    keyMap = "us";
+  };
+
   i18n.inputMethod = {
       type = "fcitx5";
       enable = true;
