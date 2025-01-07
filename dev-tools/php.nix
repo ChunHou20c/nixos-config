e7d40168bfd92bd30b147ea90719b90c8ad79e70
ChunHou20c <chunhouthatbornin20c@gmail.com>
2025-01-07 23:44:12 +0800

    format code and add some webdev dependencies

--- dev-tools/php.nix
+++ dev-tools/php.nix
@@ -2,8 +2,8 @@
 
   environment.systemPackages = with pkgs; [
 
-    php
     phpactor
+    postman
 
   ];
 }
--- flake.nix
+++ flake.nix
@@ -4,9 +4,7 @@
   nixConfig = {
     # substituers will be appended to the default substituters when fetching packages
     # nix com    extra-substituters = [munity's cache server
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
@@ -32,90 +30,85 @@
 
   };
 
-  outputs = { 
-    nixpkgs, 
-    home-manager, 
-    lanzaboote,
-    ... }@inputs: {
+  outputs = { nixpkgs, home-manager, lanzaboote, ... }@inputs: {
 
     nixosConfigurations = {
 
-      lenovo-ideapad-slim-5i = let 
+      lenovo-ideapad-slim-5i = let
 
         username = "chunhou";
-        specialArgs = {inherit username;};
+        specialArgs = { inherit username; };
 
-      in  nixpkgs.lib.nixosSystem {
-        
+      in nixpkgs.lib.nixosSystem {
+
         system = "x86_64-linux";
         inherit specialArgs;
         modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/lenovo-ideapad-slim-5i
-        ./users/${username}/nixos.nix
-        ./dev-tools
-        lanzaboote.nixosModules.lanzaboote
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./hosts/lenovo-ideapad-slim-5i
+          ./users/${username}/nixos.nix
+          ./dev-tools
+          lanzaboote.nixosModules.lanzaboote
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-          home-manager.extraSpecialArgs = inputs // specialArgs;
-          home-manager.users.${username} = import ./users/${username}/home.nix;
-        }
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} =
+              import ./users/${username}/home.nix;
+          }
 
+        ];
+      };
 
-      ];
-    };
-
-      acer-aspire-e14 = let 
+      acer-aspire-e14 = let
 
         username = "chunhou";
-        specialArgs = {inherit username;};
+        specialArgs = { inherit username; };
 
-      in  nixpkgs.lib.nixosSystem {
-        
+      in nixpkgs.lib.nixosSystem {
+
         system = "x86_64-linux";
         inherit specialArgs;
         modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/acer-aspire-e14
-        ./users/${username}/nixos.nix
-        # ./dev-tools
-        # lanzaboote.nixosModules.lanzaboote
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./hosts/acer-aspire-e14
+          ./users/${username}/nixos.nix
+          # ./dev-tools
+          # lanzaboote.nixosModules.lanzaboote
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-          home-manager.extraSpecialArgs = inputs // specialArgs;
-          home-manager.users.${username} = import ./users/${username}/home.nix;
-        }
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} =
+              import ./users/${username}/home.nix;
+          }
 
+        ];
+      };
 
-      ];
-    };
-
-      minimal = let 
+      minimal = let
 
         username = "chunhou";
-        specialArgs = {inherit username;};
+        specialArgs = { inherit username; };
 
-      in  nixpkgs.lib.nixosSystem {
-        
+      in nixpkgs.lib.nixosSystem {
+
         system = "x86_64-linux";
         inherit specialArgs;
         modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-	./configuration-minimal.nix
 
-      ];
+          ./configuration-minimal.nix
+
+        ];
+      };
     };
   };
-};
 }
