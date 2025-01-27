e42b6748d391477440a134071fc072c40c60e8cd
ChunHou20c <chunhouthatbornin20c@gmail.com>
2025-01-28 00:46:14 +0800

    update tmux session script to handle error

--- home/kitty/scripts/tmux_session.sh
+++ home/kitty/scripts/tmux_session.sh
@@ -4,6 +4,10 @@ option=$(find ~/Dev/ -mindepth 1 -maxdepth 1 -type d | fzf)
 if find "$option" -maxdepth 1 -type f -name "flake.nix" | grep -q .; then
     echo "found flake.nix, entering development shell..."
     nix develop "$option"#dev --command tmux new-session -As "$(basename "$option")" -c "$option" nvim
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to enter dev shell. Entering normal shell"
+      tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
+    fi
 else
     echo "no flake.nix found, continue with normal shell..."
     tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
