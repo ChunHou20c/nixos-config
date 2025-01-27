c5858d3bd6f7717b053538d4db7d153907363e24
ChunHou20c <chunhouthatbornin20c@gmail.com>
2025-01-28 00:32:39 +0800

    update tmux session script

--- home/kitty/scripts/tmux_session.sh
+++ home/kitty/scripts/tmux_session.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 option=$(find ~/Dev/ -mindepth 1 -maxdepth 1 -type d | fzf)
-tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
+if find "$option" -maxdepth 1 -type f -name "flake.nix" | grep -q .; then
+    echo "found flake.nix, entering development shell..."
+    nix develop "$option"#dev --command tmux new-session -As "$(basename "$option")" -c "$option" nvim
+else
+    echo "no flake.nix found, continue with normal shell..."
+    tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
+fi
