04af1bbaae1b04781c656d4c01f1e36c56cfd165
ChunHou20c <chunhouthatbornin20c@gmail.com>
2025-01-28 11:17:49 +0800

    added tmux attach hotkey and fix the tmux nix integration

--- home/kitty/scripts/tmux_session.sh
+++ home/kitty/scripts/tmux_session.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+enter_devshell(){
+
+  if [ -f "flake.nix" ]; then
+		echo "found flake.nix, entering development shell..."
+		nix develop "#dev" --command "$1"
+	else
+		echo "no flake.nix found, continue with normal shell..."
+		$1
+	fi
+}
+
 option=$(find ~/Dev/ -mindepth 1 -maxdepth 1 -type d | fzf)
 if find "$option" -maxdepth 1 -type f -name "flake.nix" | grep -q .; then
+  if nix flake show "$option" | grep -q "dev"; then
     echo "found flake.nix, entering development shell..."
-    nix develop "$option"#dev --command tmux new-session -As "$(basename "$option")" -c "$option" nvim
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to enter dev shell. Entering normal shell"
-      tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
-    fi
+    tmux new-session -As "$(basename "$option")" -c "$option" nix develop "$option"#dev --command nvim
+  else 
+    echo "no shell name dev found, entering normal dev shell..."
+    nvim
+  fi
 else
     echo "no flake.nix found, continue with normal shell..."
     tmux new-session -As "$(basename "$option")" -c "$option" "nvim"
